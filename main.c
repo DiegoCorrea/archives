c519786589b1a69aada70d703194a3db56fdda32
diegocorrea <diegocorrea.cc@gmail.com>
2016-07-21 12:17:19 -0300

    adicionando arquivos binarios

--- main.c
+++ main.c
@@ -4,82 +4,52 @@
 
 # define MAXNAME 30
 static const char DIRBANDA[MAXNAME] = "files/bandas.bin";
+static const char DIRGLOBAL[MAXNAME] = "files/global.bin";
 
-unsigned int bandasGlobalID;
-unsigned int albunsGlobalID;
-unsigned int integrantesGlobalID;
-
-struct Integrantes{
-  int id;
-  char nome[MAXNAME];
-  char funcao;
-
-  Integrantes *prox;
-  Integrantes *ant;
-};
-
-struct Albuns{
-  int id;
-  char nome[MAXNAME];
-  int anoLancamento;
-  int tempo;
-  Albuns *prox;
-  Albuns *ant;
-};
+unsigned int bandasGlobalID = 0;
 
 struct Bandas{
   int id;
   char nome[MAXNAME];
-
-  Integrantes *integrantes;
-  Albuns *albuns;
-
-  Bandas *ant;
-  Bandas *prox; 
 };
+Bandas banda;
 
-void inputBandas(Bandas **);
-void listarBandas(Bandas *);
-Bandas* criarBanda(char nome[MAXNAME]);
-void salvarBanda(Bandas **, Bandas *);
-Bandas* buscarBandaNome(Bandas *, char nome[MAXNAME]);
-void alterarNomeBanda(Bandas *, char nome[MAXNAME]);
-void alterarBandas(Bandas *);
+void criarBanda(char nome[MAXNAME]);
+void salvarBanda();
+void inputBandas();
 
+void listarBandas();
 
-void salvarArquivoBandas(Bandas *);
-void carregarArquivoBandas(Bandas **);
+void buscarBandaNome(char nome[MAXNAME]);
+void alterarNomeBanda(char nome[MAXNAME]);
+void alterarBandas();
+
+void salvarGlobalStatus();
+void carregarGlobalStatus();
 
 int main() {
-  Bandas *ptrBandas;
   int opcao = 9;
 
-  ptrBandas = NULL;
-  bandasGlobalID = 0;
-  albunsGlobalID = 0;
-  integrantesGlobalID = 0;
-
-  carregarArquivoBandas(&ptrBandas);
+  carregarGlobalStatus();
 
   while(opcao != 0){
     printf("1- Adicionar uma Banda\n2- Listar todas as Bandas\n0- Para sair do sistema\nDigite uma opção:\n");
     scanf("%d",&opcao);
     switch(opcao){ 
       case 0:
-        salvarArquivoBandas(ptrBandas);
         printf("Saindo do sistema\n");
       break;
     
       case 1:  
-        inputBandas(&ptrBandas);
+        inputBandas();
       break;  
 
       case 2:  
-        listarBandas(ptrBandas);
+        listarBandas();
       break;  
 
       case 3:  
-        alterarBandas(ptrBandas);
+        alterarBandas();
       break;
 
       default:
@@ -90,151 +60,104 @@ int main() {
   return 0;
 }
 
-Bandas* buscarBandaNome(Bandas *ptrBandas, char nome[MAXNAME]){
-  while((ptrBandas != NULL) && (strcasecmp(ptrBandas->nome,nome) != 0)){
-    ptrBandas = ptrBandas->prox;
-  }
+void buscarBandaNome(char nome[MAXNAME]){
 
-  return ptrBandas;
 }
 
-void alterarNomeBanda(Bandas *banda, char nome[MAXNAME]){
-  Bandas *tmp;
+void alterarNomeBanda(char nome[MAXNAME]){
   FILE *arquivo;
   int id;
-  /*
-  arquivo = fopen(DIRBANDA, "r");
-  if(arquivo != NULL){
-    while(fscanf(arquivo, "'%d',' %[^']\n';\n", &id,nome) != EOF){
-      printf("%d %s\n", id, nome);
-      banda = criarBanda(nome);      
-    }
-    fclose(arquivo);
-  } else {
-    printf("Erro ao tentar carregar o arquivo de Bandas!\n");
-  }
-  */
-  strcpy(banda->nome,nome);
+
+  strcpy(banda.nome,nome);
 }  
 
-void alterarBandas(Bandas *ptrBandas){
-  Bandas *banda;
+void alterarBandas(){
   char nome[MAXNAME]; 
 
   printf("Digite o nome da banda que deseja procurar:\n");
   scanf(" %[^\n]s", nome);
 
-  banda = buscarBandaNome(ptrBandas,nome);
+  buscarBandaNome(nome);
 
-  if(banda == NULL){
-    printf("Banda '%s' não encontrada\n", nome);
-    return;
-  }
-  printf("Deseja trocar o nome de %s por? \n", banda->nome);
+  printf("Deseja trocar o nome de %s por? \n", banda.nome);
   scanf(" %[^\n]s", nome);
 
-  alterarNomeBanda(banda,nome);
+  alterarNomeBanda(nome);
 }  
 
-Bandas* criarBanda(char nome[MAXNAME]){
-  Bandas *banda;
-  
-  banda = (Bandas*)malloc(sizeof(Bandas));
-
-  strcpy(banda->nome,nome);
-  
-  banda->integrantes = NULL;
-  banda->albuns = NULL;
-  banda->ant = NULL;
-  banda->prox = NULL; 
-
-  return banda;
+void criarBanda(char nome[MAXNAME]){
+  strcpy(banda.nome,nome);
+  banda.id = bandasGlobalID;  
+  bandasGlobalID++;
 }
 
-void salvarBanda(Bandas **ptrBandas, Bandas *banda){
-  Bandas *tmp;
+void salvarBanda(){
+  FILE *arquivo;
 
-  if(*ptrBandas == NULL){
-    *ptrBandas = banda;  
-  }else{
-    tmp = *ptrBandas;
-    while(tmp->prox != NULL)
-      tmp = tmp->prox;
+  if((arquivo = fopen(DIRBANDA, "a")) == NULL) {
+    printf("Erro na abertura do arquivo");
+    exit(1);    
+  }
+  if(fwrite(&banda, sizeof(struct Bandas), 1,arquivo) != 1)
+    printf("Erro na escrita do arquivo");
 
-    tmp->prox = banda;
-    banda->ant = tmp;
-
-    bandasGlobalID++;
-    banda->id = bandasGlobalID;
-  }  
+  fclose(arquivo);     
 }
 
-void inputBandas(Bandas **ptrBandas){
+void inputBandas(){
   char nome[MAXNAME];
 
   printf("Digite o nome da banda: ");
   scanf(" %[^\n]s", nome);
   printf("\n");
 
-  salvarBanda(ptrBandas, criarBanda(nome));
+  criarBanda(nome);
+  salvarBanda();
+  salvarGlobalStatus();
   printf("***Banda %s Salva com Sucesso\n", nome);
 }
 
-void listarBandas(Bandas *ptrBandas){
-  if(ptrBandas == NULL){
-    printf("Nenhuma banda registrada\n");
-  }else{
-    printf("Lista de Bandas\n");
-    while(ptrBandas != NULL ){
-      printf("---------------\n");
-      printf("id: %d\n", ptrBandas->id);
-      printf("Nome: %s\n", ptrBandas->nome);
+void listarBandas(){
+  FILE *arquivo;
 
-      ptrBandas = ptrBandas->prox;
-    }
-    printf("---------------\n");
+  printf("\t\tListando todas as Banda***\n");
+  if((arquivo = fopen(DIRBANDA, "r")) == NULL) {
+    printf("Erro na abertura do arquivo");
+    exit(1);    
   }
+  
+  rewind(arquivo); 
+
+  while(fread(&banda, sizeof(struct Bandas), 1,arquivo) == 1){
+    printf("------======------\n");
+    printf("ID: %d\nNome: %s\n", banda.id,banda.nome);
+  }
+  fclose(arquivo); 
 }
 
-void salvarArquivoBandas(Bandas *ptrBandas){
+void carregarGlobalStatus(){
   FILE *arquivo;
-  arquivo = fopen(DIRBANDA, "w");
-  while(ptrBandas != NULL){
-   fprintf(arquivo, "'%d','%s';\n", ptrBandas->id,ptrBandas->nome);
-   ptrBandas = ptrBandas->prox;
+
+  if((arquivo = fopen(DIRGLOBAL, "r")) == NULL) {
+    printf("Erro na abertura do arquivo");
+    return;   
   }
-  fclose(arquivo);
+  
+  if(fread(&bandasGlobalID, sizeof(unsigned int), 1,arquivo) != 1)
+    printf("Erro na abertura do arquivo\n");
+
+  fclose(arquivo); 
 }
 
-void carregarArquivoBandas(Bandas **ptrBandas){
-  Bandas *banda, *tmp;
+void salvarGlobalStatus(){
   FILE *arquivo;
-  int id;
-  char nome[MAXNAME]; 
 
-  arquivo = fopen(DIRBANDA, "r");
-  if(arquivo != NULL){
-    while(fscanf(arquivo, "'%d',' %[^']\n';\n", &id,nome) != EOF){
-      banda = criarBanda(nome);
-
-      if(*ptrBandas == NULL){
-        *ptrBandas = banda;  
-      }else{
-        tmp = *ptrBandas;
-        while(tmp->prox != NULL)
-          tmp = tmp->prox;
-
-        tmp->prox = banda;
-        banda->ant = tmp;
-
-        banda->id = id;
-        if(bandasGlobalID < id){
-          bandasGlobalID = id;
-        }  
-      }  
-    }
-    fclose(arquivo);
-  } else {
-    printf("Erro ao tentar carregar o arquivo de Bandas!\n");
+  if((arquivo = fopen(DIRGLOBAL, "w")) == NULL) {
+    printf("Erro na abertura do arquivo");
+    exit(1);    
   }
+  if(fwrite(&bandasGlobalID, sizeof(unsigned int), 1,arquivo) != 1)
+    printf("Erro na escrita do arquivo");
+
+  fclose(arquivo);     
 }
